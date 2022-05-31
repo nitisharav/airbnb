29f185d5c98dd27a4a9c14111938eaedbc5ec41f
Nitish Kumar <nitisharav@gmail.com>
2022-05-31 08:36:33 +0530

    Airbnb

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+// import Katie from "./images/katie_pic.png";
+import data from "./data";
 
 function App() {
+  const card_data = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        item={item}
+        //{...item}
+        // src={`/images/${data.coverImg}`}
+        // rating={data.stats.rating}
+        // reviewCount={data.stats.reviewCount}
+        // location={data.location}
+        // title={data.title}
+        // price={data.price}
+        // openSpots={data.openSpots}
+      />
+    );
+  });
+  // console.log(card_data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      {/* <Card
+        src="/images/katie_pic.png"
+        rating="5.0"
+        reviewCount={6}
+        location="USA"
+        title="Life lessons with Katie Zaferes"
+        price={136}
+      /> */}
+      <section className="cards--list">{card_data}</section>
     </div>
   );
 }
